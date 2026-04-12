f621ccaff12949d79da789a8352fbf1707a43809
Edis Mutluer <info@letmebowl-catering.de>
2026-04-12 20:14:43 +0200

    fix server import for react-router express

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { createRequestHandler } from "@react-router/node";
+import { createRequestHandler } from "@react-router/express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const build = await import("./build/server/index.js");
 
-app.all("*", createRequestHandler({
-  build: await import("./build/server/index.js")
-}));
+app.use(express.static("build/client"));
+
+app.all("*", createRequestHandler({ build }));
 
 app.listen(PORT, () => {
-  console.log("Server läuft auf Port", PORT);
+  console.log(`Server läuft auf Port ${PORT}`);
 });
